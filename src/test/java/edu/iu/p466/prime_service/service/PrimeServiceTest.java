2479e849f121b4ac5187224cff1f0103f66d40f7
LukeCali-949 <mengluke17@gmail.com>
2025-02-20 16:56:46 -0500

    fixign test

--- src/test/java/edu/iu/p466/prime_service/service/PrimeServiceTest.java
+++ src/test/java/edu/iu/p466/prime_service/service/PrimeServiceTest.java
@@ -12,6 +12,15 @@ class PrimeServiceTest {
     void _23IsPrime() {
 
         int n = 23;
+        boolean expected = true;
+        boolean acutal = primeService.isPrime(n);
+        assertEquals(expected,acutal);
+    }
+
+    @Test
+    void _24IsPrime() {
+
+        int n = 24;
         boolean expected = false;
         boolean acutal = primeService.isPrime(n);
         assertEquals(expected,acutal);
